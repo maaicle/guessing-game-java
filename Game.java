fcd563e9ac29de00cca9115009922821d10fc75b
Michael Hardcastle <maaicle@gmail.com>
2021-12-09 22:09:17 -0700

    added getUserName method

--- Game.java
+++ Game.java
@@ -4,9 +4,20 @@ A number-guessing game.
 
 */
 
+import java.util.Scanner;
+
 public class Game {
+  static String player;
+
   public static void main(String[] args){
     System.out.println("Hello Player");
+    getUserName();
+  }
+
+  static void getUserName() {
+    System.out.println("What's yo name?");
+    Scanner scanner = new Scanner(System.in);
+    player = scanner.nextLine();
   }
 
 }
